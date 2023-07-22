2f247380859c3a505f37f51e7c4bf657d4aa4bc5
Sai H <72930376+saih1@users.noreply.github.com>
2023-07-23 00:07:34 +1000

    Creates List contents for List screen

--- app/src/main/java/com/example/propertyapp/view/screens/ListScreen.kt
+++ app/src/main/java/com/example/propertyapp/view/screens/ListScreen.kt
@@ -2,20 +2,23 @@
 
 package com.example.propertyapp.view.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,20 +28,75 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.propertyapp.R
 
 @Composable
-fun PropertyInformation() {
+fun PropertyItemComposable(
+    modifier: Modifier = Modifier, onClick: () -> Unit
+    // TODO: View data as arg
+) {
+    Card(
+        onClick = { onClick() },
+        modifier = modifier.padding(15.dp),
+        shape = RoundedCornerShape(25.dp)
+    ) {
+        AsyncImage(
+            model = "https://images.pexels.com/photos/2119713/pexels-photo-2119713.jpeg",
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .padding(top = 15.dp, start = 15.dp, end = 15.dp)
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(25.dp)),
+            // TODO: Need to replace placeholder resource
+            placeholder = painterResource(id = R.drawable.ic_launcher_background),
+            error = painterResource(id = R.drawable.ic_launcher_background)
+        )
+        PropertyContent(modifier = Modifier.padding(horizontal = 15.dp))
+    }
+}
+
+@Composable
+fun PropertyContent(
+    modifier: Modifier = Modifier
+    //TODO: Args
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier.weight(1f)
+        ) {
+            PropertyInformation(
+                modifier = Modifier.weight(0.4f)
+            )
+            AgentProfile(
+                modifier = Modifier.weight(0.1f),
+                agentName = "John Smith",
+                agentAvatarUrl = "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg"
+            )
+        }
+    }
+}
+
+@Composable
+fun PropertyInformation(
+    modifier: Modifier = Modifier
+) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.Start,
-        modifier = Modifier.padding(5.dp)
+        modifier = modifier.padding(5.dp)
     ) {
         // Agent company
         Text(
@@ -117,25 +175,33 @@ fun RoomSpaceQuantityRow(
 
 @Composable
 fun AgentProfile(
-    agentName: String, agentAvatar: String
+    agentName: String, agentAvatarUrl: String, modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(5.dp)
+        horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.padding(5.dp)
     ) {
-        Box(modifier = Modifier.wrapContentSize()) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "profile image",
-                modifier = Modifier.clip(CircleShape)
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .size(60.dp)
+        ) {
+            AsyncImage(
+                model = agentAvatarUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+                modifier = Modifier.clip(CircleShape),
+                placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                error = painterResource(id = R.drawable.ic_launcher_background)
             )
         }
+        Spacer(modifier = Modifier.size(2.dp))
         Text(
-            text = agentName, textAlign = TextAlign.Center
+            text = agentName,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelSmall
         )
     }
-
 }
 
 @Composable
@@ -156,8 +222,14 @@ fun PreviewListTopAppBar() = ListTopAppBar()
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewAgentProfile() = AgentProfile(agentName = "John Smith", agentAvatar = "")
+fun PreviewAgentProfile() = AgentProfile(
+    agentName = "John Smith", agentAvatarUrl = ""
+)
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewPropertyInformation() = PropertyInformation()
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPropertyItemComposable() = PropertyItemComposable {}
